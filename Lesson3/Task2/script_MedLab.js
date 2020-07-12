19d3c706ae822099fa977a7142f669c5391ef0e0
kseniya-chepur <64986545+kseniya-chepur@users.noreply.github.com>
2020-07-12 22:08:32 +0300

    Update script_MedLab.js

--- Lesson3/Task2/script_MedLab.js
+++ Lesson3/Task2/script_MedLab.js
@@ -1,5 +1,6 @@
 //--Завантажити з мережі будь-який шаблон сайту (index_MedLab.html)
 //Підключити до нього свій скріпт-файл. У файлі прописати наступні доступи та дії
+//- знайти всі елементі, які мають class
 let listOfClass = document.querySelectorAll('[class]');
 for (let item of listOfClass) {
     console.log(item);
