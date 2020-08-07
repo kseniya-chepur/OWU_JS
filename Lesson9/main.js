35dcb0a99350614ec53a962b1bb8f2e553d64f6c
kseniya.chepur <chepur_@ukr.net>
2020-08-07 21:34:21 +0300

    Initial commit

--- Lesson9/main.js
+++ Lesson9/main.js
@@ -10,6 +10,8 @@
 
 // Якщо щось пішло не так (нема шо їсти), то має бути викинута помилка і решта функцій виконуватись не мають.
 
+//callback
+
 function getUp(isWakingUpTrue, cb) {
     setTimeout(() => {
         if(isWakingUpTrue) {            
@@ -70,9 +72,9 @@ function getSalary(isYourCreditCardWithYou, cb) {
         }, 1000);
 }
 
-function meetYourFriend(didYourFriendConfirmed, cb) {
+function meetYourFriend(didYourFriendConfirm, cb) {
     setTimeout(() => {
-        if(didYourFriendConfirmed) {
+        if(didYourFriendConfirm) {
             cb(null, 'ok');
         } else {
             cb('err', null);
@@ -94,7 +96,7 @@ getUp(true, (err, ok) => {
                         if(err) {
                             console.log('You are late');
                         } else {
-                            console.log('You got to work on time!');
+                            console.log('You will get to work on time!');
                             goToMeeting(true, (err, ok) => {
                                 if(err) {
                                     console.log('Report is not ready');
@@ -130,3 +132,144 @@ getUp(true, (err, ok) => {
     }
 });
 
+//Promise 
+
+function getUp1(isWakingUpTrue) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+        if(isWakingUpTrue) {            
+            resolve('Good start of the day!');
+        } else {
+            reject('You overslept');
+            }
+        }, 1000);
+})
+}
+
+function cookBreakfast1(isFoodAvailable) {
+    return new Promise((resolve, reject) => {
+    setTimeout(() => {
+        if(isFoodAvailable) {            
+           resolve('Have your breakfast!');
+        } else {
+            reject('no food');
+            }
+        }, 1000);
+})
+}
+
+function getToWork1(catchTheBus) {
+return new Promise((resolve, reject) => {
+    setTimeout(() => {
+        if(catchTheBus) {            
+           resolve('You will get to work on time!');
+        } else {
+            reject('You missed the bus');
+            }
+        }, 1000);
+})
+}
+    
+function goToMeeting1(isReportReady) {
+return new Promise((resolve, reject) => {
+    setTimeout(() => {
+        if(isReportReady) {            
+           resolve('You are ready for the meeting!');
+        } else {
+            reject('Report is not ready');
+            }
+        }, 1000);
+})
+}
+        
+function callYourClient1(isClientAvailable) {
+return new Promise((resolve, reject) => {
+    setTimeout(() => {
+        if(isClientAvailable) {            
+           resolve('You reached your client!');
+        } else {
+            reject('Client does not answer your call');
+            }
+        }, 1000);
+})
+}
+            
+function getSalary1(isYourCreditCardWithYou) {
+return new Promise((resolve, reject) => {
+    setTimeout(() => {
+        if(isYourCreditCardWithYou) {            
+           resolve('You got your salary!');
+        } else {
+            reject('You cannot get salary without the credit card');
+            }
+        }, 1000);
+})
+}
+
+function meetYourFriend1(didYourFriendConfirm) {
+return new Promise((resolve, reject) => {
+    setTimeout(() => {
+        if(didYourFriendConfirm) {            
+           resolve('Enjoy time with your friend!');
+        } else {
+            reject('You friend did not confirm your meeting');
+            }
+        }, 1000);
+})
+}
+  
+getUp1(true).then(result => {
+    console.log(result);
+    return cookBreakfast1(true);
+})
+.then(result => {
+    console.log(result);
+    return getToWork1(true);
+})
+.then(result => {
+    console.log(result);
+    return goToMeeting1(true);
+})
+.then(result => {
+    console.log(result);
+    return callYourClient1(true);
+})
+.then(result => {
+    console.log(result);
+    return getSalary1(true);
+})
+.then(result => {
+    console.log(result);
+    return meetYourFriend1(true);
+})
+.then(result => {
+    console.log(result);
+})    
+.catch(reason => {
+    console.log(reason);
+});
+
+//async/await
+ 
+async function yourDay() {
+    try {    
+        let result1 = await getUp1(true);
+        console.log(result1);
+        let result2 = await cookBreakfast1(false);
+        console.log(result2);
+        let result3 = await getToWork1(true);
+        console.log(result3);
+        let result4 = await goToMeeting1(true);
+        console.log(result4);
+        let result5 = await callYourClient1(true);
+        console.log(result5);
+        let result6 = await getSalary1(true);
+        console.log(result6);
+        let result7 = await meetYourFriend1(true);
+        console.log(result7);
+    } catch(e) {
+        console.error(e);
+    }
+}
+
+yourDay();
