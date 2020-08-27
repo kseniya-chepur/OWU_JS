15836bc638917ef70eafb00433666ac33e07c678
kseniya-chepur <64986545+kseniya-chepur@users.noreply.github.com>
2020-08-27 16:28:45 +0300

    Initial commit

---
+++ task2/src/components/TodoItem.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <span>{{item}}</span>
+        <button @click="Delete">Delete</button>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'TodoItem',
+    props: {
+        item: {
+            type: String,
+            required: true
+        }
+    },
+
+    methods: {
+        Delete() {            
+            this.$emit('DeleteFromList', this.item)
+        }
+    }    
+}
+</script>
+
+<style scoped>
+    span {
+        margin: 20px;
+    }
+</style>
